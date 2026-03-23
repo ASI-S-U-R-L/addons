a5a7dffb55f19fa1dacea77954932c174db16e8b
Javier Escobar <107713929+jescobarperez@users.noreply.github.com>
2026-03-23 01:43:57 -0400

    ok

--- calendar_workplan/models/__init__.py
+++ calendar_workplan/models/__init__.py
@@ -4,7 +4,7 @@ from . import gamification_challenge
 from . import calendar_workplan_section
 from . import res_company
 from . import calendar_workplan_plan
-from . import calendar_event
 from . import calendar_recurrence
+from . import calendar_event
 from . import calendar_event_completion
 from . import debug_recurrence_hook
--- calendar_workplan/models/calendar_recurrence.py
+++ calendar_workplan/models/calendar_recurrence.py
@@ -10,21 +10,14 @@ class CalendarRecurrence(models.Model):
     _inherit = 'calendar.recurrence'
 
     def _get_recurrent_dates(self, base_event):
-        """
-        Recorta silenciosamente las fechas generadas por la recurrencia
-        para que NO pasen del año del evento base.
-        """
-        # 1) Obtener fechas originales usando el método real de Odoo
         dates = super()._get_recurrent_dates(base_event)
 
         if not base_event.start:
             return dates
 
-        # 2) Año del evento base
         base_year = base_event.start.year
         limit_dt = datetime(base_year, 12, 31, 23, 59, 59)
 
-        # 3) Recortar
         filtered = [dt for dt in dates if dt <= limit_dt]
 
         if len(filtered) != len(dates):
