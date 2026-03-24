c9361521522f3bb15c09a4c97f752421b3148472
Javier Escobar <107713929+jescobarperez@users.noreply.github.com>
2026-03-24 12:45:50 -0400

    Update calendar_recurrence.py

--- calendar_workplan/models/calendar_recurrence.py
+++ calendar_workplan/models/calendar_recurrence.py
@@ -9,11 +9,16 @@ _logger = logging.getLogger(__name__)
 class CalendarRecurrence(models.Model):
     _inherit = 'calendar.recurrence'
 
-    def _get_rrule(self):
-        rrule = super()._get_rrule()
+    def _get_rrule(self, dtstart=None):
+        # Llamar al método original con el mismo argumento
+        rrule = super()._get_rrule(dtstart=dtstart)
+
+        # Si hay evento base, recortar con UNTIL
         if self.base_event_id and self.base_event_id.start:
             limit_dt = datetime(self.base_event_id.start.year, 12, 31, 23, 59, 59)
-            rrule._until = limit_dt  # fuerza límite en la regla
+            # Forzar límite en la regla
+            rrule._until = limit_dt
+
         return rrule
 
     def _get_recurrent_dates(self, base_event):
