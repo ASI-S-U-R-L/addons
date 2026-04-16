728a0371c3a5b3b3641885079bccf8abf1b3a189
Javier Escobar <107713929+jescobarperez@users.noreply.github.com>
2026-04-16 13:44:21 -0400

    Update account_move.py

--- asi_custom_sale/models/account_move.py
+++ asi_custom_sale/models/account_move.py
@@ -67,15 +67,26 @@ class AccountMove(models.Model):
     def action_set_gestion_state_archived(self):
         self._set_gestion_state('archived')
 
+
     @api.depends('invoice_line_ids.analytic_distribution')
     def _compute_analytic_accounts(self):
         for move in self:
             cuentas = self.env['account.analytic.account']
+
             for line in move.invoice_line_ids:
                 if line.analytic_distribution:
                     analytic_ids = [int(x) for x in line.analytic_distribution.keys()]
-                    cuentas |= self.env['account.analytic.account'].browse(analytic_ids)
+
+                    cuentas_linea = self.env['account.analytic.account'].search([
+                        ('id', 'in', analytic_ids),
+                        ('plan_id.name', '=', 'Empleados')
+                    ])
+
+                    cuentas |= cuentas_linea
+
             move.analytic_accounts_ids = cuentas
+
+
 
 
 
